02fa587ec1cefb6625b23bd0ab9535e8465d4ffb
JASON YANG <yangyb2005@outlook.com>
2026-09-04 18:36:49 -0700

    CHIPS2.5 finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,78 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  s = 0
+  arr.each do |x|
+    s += x
+  end
+  s
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr.length == 0
+    0
+  elsif arr.length == 1
+    arr[0]
+  else
+    sorted_arr = arr.sort
+    sorted_arr[-1] + sorted_arr[-2]
+  end
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+  0.upto(arr.length - 1) do |x|
+    t = arr.length - x
+    1.upto(t - 1) do |y|
+       return true if arr[x] + arr[x + y] == n
+      end
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  return false unless s[0] =~ /[a-z]/i
+  return false if s[0] =~ /[aeiou]/i
+  true
 end
 
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  return false unless s =~ /^(0|1)+$/
+  return false unless s =~ /.*0{2}$/ || s == '0'
+  true
 end
 
 # Part 3
 
 class BookInStock
-  # YOUR CODE HERE
+  def initialize(isbn, price)
+    raise(ArgumentError) if isbn == '' || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn
+    @isbn
+  end
+
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+
+  def price
+    @price
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
 end
